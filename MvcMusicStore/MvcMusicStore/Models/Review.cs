7f305aff9890a60d2bbca08ff5a60333a5463b11
Bronislav Draška <bronislavdraska@gmail.com>
2016-12-29 22:36:26 +0100

    second lesson - crating conrollers

---
+++ MvcMusicStore/MvcMusicStore/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+        public string Contents { get; set; }
+
+        [Required()]
+        [Display(Name = "Email address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}
